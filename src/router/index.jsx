2464882152098d518d10aec395e9c18317aa3d3b
Uche Njoku <uche.njoku@day3.com.au>
2025-07-19 18:35:56 +1000

    update route

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -5,95 +5,15 @@ import {
 } from "react-router-dom";
 //Pages
 
-import Home from '../pages/Home';
-import Contacts from '../pages/Contacts';
-import Stories from '../pages/Stories';
-import Profile from '../pages/Profile';
-import Chatbot from '../pages/Chatbot';
-import Chatbot2 from '../pages/Chatbot2';
+
 import Welcome from '../pages/Welcome';
-import Pricing from '../pages/Pricing';
-import Faq from '../pages/Faq';
+
 import NotFound from '../pages/NotFound';
-
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Forgot from '../pages/Forgot';
-
-import UiElements from '../pages/UiElements';
-import UiChatReplies from '../pages/UiChatReplies';
-import UiUsecaseModals from '../pages/UiUsecaseModals';
-
-import Landing from '../pages/Landing';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/chats",
-    element: <Home />,
-  },
-  {
-    path: "/contacts",
-    element: <Contacts />,
-  },
-  {
-    path: "/stories",
-    element: <Stories />,
-  },
-  {
-    path: "/chatbot",
-    element: <Chatbot />,
-  },
-  {
-    path: "/chatbot-s2",
-    element: <Chatbot2 />,
-  },
-  {
-    path: "/chatbot-welcome",
     element: <Welcome />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
-  },
-  {
-    path: "/pricing",
-    element: <Pricing />,
-  },
-  {
-    path: "/faq",
-    element: <Faq />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/forgot",
-    element: <Forgot />,
-  },
-  {
-    path: "/ui-elements",
-    element: <UiElements />,
-  },
-  {
-    path: "/ui-chat-replies",
-    element: <UiChatReplies />,
-  },
-  {
-    path: "/ui-usecase-modals",
-    element: <UiUsecaseModals />,
-  },
-  {
-    path: "/landing",
-    element: <Landing />,
   },
   {
     path: "*",
